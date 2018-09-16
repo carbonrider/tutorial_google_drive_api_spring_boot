852dc9b43241c3deee34c9cc10047daccab34f75
unknown <yogesh.jadhav@gmail.com>
2018-09-16 11:06:12 +0530

    Draft project configured for spring boot.

---
+++ drive-list-files/src/main/java/com/carbonrider/tutorials/gdrive/sbt/drivelistfiles/DriveListFilesApplication.java
@@ -0,0 +1,12 @@
+package com.carbonrider.tutorials.gdrive.sbt.drivelistfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DriveListFilesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DriveListFilesApplication.class, args);
+	}
+}
---
+++ drive-list-files/src/test/java/com/carbonrider/tutorials/gdrive/sbt/drivelistfiles/DriveListFilesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.carbonrider.tutorials.gdrive.sbt.drivelistfiles;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DriveListFilesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
